6baa0c196e4e995bb8daf41283284caadcd6ea74
teja varma <52781687+tejavarma-aln@users.noreply.github.com>
2020-05-02 18:33:39 +0530

    Add files via upload

---
+++ PdfLocker/LockerClass.cs
@@ -0,0 +1,54 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.IO;
+ 
+namespace PdfLocker
+{
+    public class LockerClass
+    {
+
+
+        public string doencrypt(string fpath,string upass,string opass)
+        {
+
+            if (!File.Exists(fpath))
+            {
+                return ("File Not Found");
+            }
+            if(String.IsNullOrEmpty(upass) || String.IsNullOrEmpty(opass))
+            {
+                return ("Invalid Security Keys");
+            }
+            string outdir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Encrypted PDFs");
+
+            if (!Directory.Exists(outdir))
+            {
+                Directory.CreateDirectory(outdir);
+            }
+
+            string outfilename = "Encrypted-" + Path.GetFileName(fpath);
+            string outfile = Path.Combine(outdir, outfilename);
+
+            try
+            {
+              
+                using (Stream input = new FileStream(fpath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    using (Stream output = new FileStream(outfile, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        PdfReader reader = new PdfReader(input);
+                        PdfEncryptor.Encrypt(reader, output, true, upass, opass, PdfWriter.ALLOW_SCREENREADERS);
+                    }
+                }
+            }catch(Exception ex)
+            {
+                return ("Error : " + ex.Message);
+            }
+
+            return "File Enctrypted at " +outfile ;
+
+
+        }
+
+    }
+}
---
+++ PdfLocker/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PdfLocker")]
+[assembly: AssemblyDescription("Encrypt PDF Files From Tally")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AppLoops")]
+[assembly: AssemblyProduct("PdfLocker")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("AppLoops")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("32dac5c5-bbb2-4c96-802b-578d9c0ed8f1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
